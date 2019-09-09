f5d746128de961e271fd2afd5a12fc9b82a40aa1
azarjod <jodieazar@gmail.com>
2019-09-09 14:15:44 -0400

    added printing class

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, world!");
+        System.out.println('This is another line.");
+
+        System.out.println("This is \"Jodie's\" computer");
+        System.out.println()
+    }
+}
