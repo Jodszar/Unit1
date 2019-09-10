0880c572480bf43f178ebfc2823ed01ae0949d89
azarjod <jodieazar@gmail.com>
2019-09-10 15:03:57 -0400

    This is a great time to be a programmer.

--- src/com/company/printing.java
+++ src/com/company/printing.java
@@ -3,5 +3,14 @@ package com.company;
 public class printing {
     public static void main(String[] args) {
 
+        System.out.println("2:53");
+        System.out.println("9/10/19");
+        System.out.println("This is a great");
+        System.out.println("Time to be a");
+        System.out.println("Computer programmer!");
+        System.out.print("This is a great");
+        System.out.print(" Time to be a");
+        System.out.print("computer programmer!");
+
     }
 }
