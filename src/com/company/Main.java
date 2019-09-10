b0d93bde325b9454e77b318a26208cd90090e06f
azarjod <jodieazar@gmail.com>
2019-09-10 14:39:51 -0400

    Abdating from yesterday

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, world!");
-        System.out.println('This is another line.");
+        System.out.println("This is another line.");
 
         System.out.println("This is \"Jodie's\" computer");
-        System.out.println()
     }
 }
