bf91c81b3843db4c848ce894785dda1493a33b4a
azarjod <jodieazar@gmail.com>
2019-09-12 08:49:08 -0400

    Escape

---
+++ src/com/company/Escape.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Escape {
+           public static void main(String[] arges) {
+
+               System.out.println("What is the difference between ");
+               System.out.println("a'and a \"? or between a\" and a\\\"?");
+               System.out.println("One is what we see when we're typing our program");
+               System.out.println("The other is what appears on the \"console.\"");
+
+
+           }
+}
