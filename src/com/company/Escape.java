dc680954d39343a1f07b83a638f20885ce2e7af2
azarjod <jodieazar@gmail.com>
2019-09-12 09:00:20 -0400

    Escape 2

--- src/com/company/Escape.java
+++ src/com/company/Escape.java
@@ -8,6 +8,13 @@ public class Escape {
                System.out.println("One is what we see when we're typing our program");
                System.out.println("The other is what appears on the \"console.\"");
 
+               System.out.println("  \\/");
+               System.out.println(" \\\\//");
+               System.out.println("\\\\\\///");
+               System.out.println("///\\\\\\");
+               System.out.println(" //\\\\");
+               System.out.println("  /\\");
+
 
            }
 }
