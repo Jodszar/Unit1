96e390a202fe4175d6cc0124f1b23be3f387be1d
azarjod <jodieazar@gmail.com>
2019-09-16 11:14:13 -0400

    TotalWithTax

---
+++ src/com/company/TotalWithTax.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class TotalWithTax {
+    public static void main(String[] args) {
+
+        final double TAXRATE= 0.0825;
+        double price= 52.75;
+        double tax= price*TAXRATE;
+        double totalCost= price + tax;
+
+        System.out.println("Price: "+price);
+        System.out.println("Sales Tax: " +"$"+tax);
+        System.out.println("Total: " +"$"+totalCost);
+
+
+
+    }
+}
