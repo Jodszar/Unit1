377bbe53f11fc40321600d4ee880395194a907dc
azarjod <jodieazar@gmail.com>
2019-09-16 10:51:00 -0400

    Rectanglearea

---
+++ src/com/company/Rectanglearea.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Rectanglearea {
+    public static void main(String[] args) {
+
+        int length= 14;
+        int width= 6;
+        int area= length*width;
+
+        System.out.println("Area="+area);
+
+
+
+    }
+}
