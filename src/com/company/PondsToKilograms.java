a0e6a59e35ebb49835ed8b0f9843487efbce38c7
azarjod <jodieazar@gmail.com>
2019-09-16 11:02:06 -0400

    PoundsToKilograms

---
+++ src/com/company/PondsToKilograms.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class PondsToKilograms {
+    public static void main(String[] args) {
+
+        double lbs= 115;
+        double kg= lbs/2.205;
+
+        System.out.print( lbs+" pound = ");
+        System.out.print(kg);
+
+    }
+}
