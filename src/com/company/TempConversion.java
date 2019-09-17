2ce5694be1c6eca201af0ce131c836ee76bf84d2
azarjod <jodieazar@gmail.com>
2019-09-17 12:50:22 -0400

    TempConversion

---
+++ src/com/company/TempConversion.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TempConversion {
+    public static void main(String[] args) {
+
+        double fahrenheit= 79;
+        double Celsius= ((fahrenheit-32)*5.0/9);
+
+        System.out.print(+fahrenheit+"Fahrenheit= ");
+        System.out.println( +Celsius+ "Celsius");
+
+
+
+    }
+}
